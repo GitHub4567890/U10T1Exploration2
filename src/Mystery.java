9f6845e480bb5e00ce468079378a6149149e67af
BT_3W2_27 <ibtehazr@nycstudents.net>
2023-04-18 12:34:03 -0400

    a

--- src/Mystery.java
+++ src/Mystery.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Mystery {
+public class Mystery
+{
+    public static void main(String [] args)
+    {
+        int someNum = mystery(9, 2);
+        System.out.println(someNum);
+    }
+
+    public static int mystery(int m, int n)
+    {
+        if (m <= 0)
+        {
+            return n;
+        }
+        return n * mystery(m - 2, n + 1);
+    }
 }
